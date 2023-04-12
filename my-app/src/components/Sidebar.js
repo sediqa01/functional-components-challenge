0c09c8e470a78d3a0b290d5ba764951447c19e3d
sediqa01 <>
2023-04-12 13:46:50 +0000

    Arrow function

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,14 +1,12 @@
 import React from "react";
 
 
-  function Sidebar() {
-    return (
+ const Sidebar = () => {
         <div>
             <a href = "#." target= "_blank">My Photos</a>
             <a href = "#." target= "_blank">My Illustrations</a>
             <a href = "#." target= "_blank">My Paintings</a>
         </div>
-    )
 }
 
 export default Sidebar;
